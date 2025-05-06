1767d55f879841ad4c23967a8c0ab8663b745846
yudi-star <yudith.pacco@tecsup.edu.pe>
2025-05-06 13:38:34 -0500

    serializers creados

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Producto, Pedido, DetallePedido
 
-# Register your models here.
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'stock', 'created_at')
+    search_fields = ('nombre',)
+    list_filter = ('created_at',)
+
+class DetallePedidoInline(admin.TabularInline):
+    model = DetallePedido
+    extra = 1
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'fecha_pedido', 'estado', 'total')
+    list_filter = ('estado', 'fecha_pedido')
+    search_fields = ('usuario__username',)
+    inlines = [DetallePedidoInline]
